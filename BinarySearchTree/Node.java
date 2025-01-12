5cc8a05689a3b674b79778a4f81d7239b9925785
Chandan Mohanty <116899402+Cmohanty26@users.noreply.github.com>
2025-01-12 21:19:03 +0530

    BinarySearch Tree Program

---
+++ BinarySearchTree/Node.java
@@ -0,0 +1,18 @@
+package com.BinarySearchTree;
+
+public class Node {
+	int key;
+	Node left;
+	Node right;
+	public Node(int key) {
+		this.key=key;
+	}
+	public Node(int key, Node left, Node right) {
+		super();
+		this.key = key;
+		this.left = left;
+		this.right = right;
+	}
+	
+
+}
---
+++ BinarySearchTree/Test.java
@@ -0,0 +1,14 @@
+package com.BinarySearchTree;
+
+public class Test {
+
+	public static void main(String[] args) {
+		BinarySearchTree b=new BinarySearchTree();
+		b.add(20);
+		b.add(60);
+		b.add(80);
+		System.out.println(b);
+
+	}
+
+}
